f1b9348fd4c25947b1d751a4cd8ea1bbb4b4ebc3
ShadowXCC <shadowxcc@gmail.com>
2020-06-01 14:21:18 -0400

    Removed Unneeded Import

--- TeneCryptGUI/src/invalidKeyPopup.java
+++ TeneCryptGUI/src/invalidKeyPopup.java
@@ -3,7 +3,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
